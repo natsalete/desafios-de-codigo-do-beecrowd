4e28a64bc1e7e7de55628607b40f433862bc1d86
natsalete <natsalete14@gmail.com>
2023-09-01 21:36:20 -0300

    1000-1001-1013

---
+++ beecrowd-iniciante/1001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+//C++20
+int main() {
+    
+    int A, B, X;
+    scanf("%d %d", &A, &B);
+    
+    X = A + B;
+    
+    printf("X = %d\n", X);
+    
+    
+    return 0;
+}
