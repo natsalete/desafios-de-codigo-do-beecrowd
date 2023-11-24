fd897f79420b597ab7139cb35cb05c66542b8d2b
natalete <natsalete14@gmail.com>
2023-11-24 12:51:21 -0300

    Busca na Internet

---
+++ beecrowd-iniciante/2413 - Busca na Internet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main() {
+    
+    int t, link1, link2;
+    
+    scanf("%d", &t);
+    
+    link2 = 2 * t;
+    link1 = 2 * link2;
+    
+    printf("%d\n", link1);
+   
+ 
+    return 0;
+}
