e43461efbfb437d429d8460215c064c79fd69fc1
natalete <natsalete14@gmail.com>
2024-01-08 15:45:12 -0300

    Soma de Ímpares Consecutivos II

---
+++ beecrowd-iniciante/1099 - Soma de Ímpares Consecutivos II.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+
+    while(n--){
+        int x, y, soma = 0;
+
+        cin >> x >> y;
+
+        if(x < y){
+            for(int i = x+1; i < y; i++){
+               if(i % 2 != 0){
+                soma += i;
+               }
+            }
+        }else {
+            for(int i = y+1; i < x; i++){
+               if(i % 2 != 0){
+                soma += i;
+               }
+            }
+        }
+        cout << soma << endl;
+    }
+    return 0;
+}
