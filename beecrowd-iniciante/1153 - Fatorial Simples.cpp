5613e61da35e2330e21266c3b5a9b56ecfa29b80
natalete <natsalete14@gmail.com>
2024-01-09 13:29:16 -0300

    Fatorial Simples

---
+++ beecrowd-iniciante/1153 - Fatorial Simples.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int fatorial(int n){
+    if(n <= 1){
+        return 1;
+    }
+    return n * fatorial(n - 1);
+}
+
+
+
+int main(){
+    int n;
+
+    cin >> n;
+
+    cout << fatorial(n) << endl;
+
+    return 0;
+}
