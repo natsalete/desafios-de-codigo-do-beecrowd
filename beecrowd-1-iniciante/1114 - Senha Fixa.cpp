2f9b802ed025b64abec7e0f1e276dde70cfcf6cf
natalete <natsalete14@gmail.com>
2024-02-01 13:03:02 -0300

    Senha Fixa

---
+++ beecrowd-1-iniciante/1114 - Senha Fixa.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int senha;
+
+    while(cin >> senha){
+        if(senha == 2002){
+            cout << "Acesso Permitido"<< endl;
+            break;
+        }else{
+            cout << "Senha Invalida" << endl;
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
