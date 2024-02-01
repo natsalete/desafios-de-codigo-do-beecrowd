16f2a6347645b13bac5e817b869390e4cef94ea8
natalete <natsalete14@gmail.com>
2024-02-01 12:09:09 -0300

    Sequencia IJ 2

---
+++ beecrowd-1-iniciante/1096 - Sequencia IJ 2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int j1 = 7;
+    int j2 = 6;
+    int j3 = 5;
+
+    for(int i = 1; i <= 9; i += 2){
+        cout <<"I=" << i <<  " J=" << j1 << endl;
+        cout <<"I=" << i <<  " J=" << j2 << endl;
+        cout <<"I=" << i <<  " J=" << j3 << endl;
+
+    }
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
