467be5be6e2be351c758604de631e0475ab27a7c
natalete <natsalete14@gmail.com>
2024-02-02 15:19:50 -0300

    Linha na Matriz

---
+++ beecrowd-1-iniciante/1181 - Linha na Matriz.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int l;
+
+    cin >> l;
+
+    char t;
+
+    cin >> t;
+
+    float matriz[12][12], soma = 0, cont = 0;
+
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            cin >> matriz[i][j];
+            if(i == l){
+                soma += matriz[i][j];
+                cont++;
+            }
+        }
+    }
+
+    cout.precision(1);
+    cout.setf(ios::fixed);
+
+    if (t == 'S'){
+        cout << soma << endl;
+    }else {
+        cout << soma / cont << endl;
+    }
+    return 0;
+}
+
