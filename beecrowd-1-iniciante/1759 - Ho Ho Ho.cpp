a1c6fe3f282448f4f42d9daf3e452d58afe53f4e
natalete <natsalete14@gmail.com>
2024-02-05 17:33:50 -0300

    Ho Ho Ho

---
+++ beecrowd-1-iniciante/1759 - Ho Ho Ho.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+
+    int aux = n-1;
+
+    while (aux--){
+        cout << "Ho ";
+    }
+
+    cout << "Ho!" << endl;
+
+    return 0;
+}
+
+
+
+
+
