f9c856fb8398937eef0538298aee255f76653be2
natalete <natsalete14@gmail.com>
2024-02-05 17:30:31 -0300

    Bob Conduite

---
+++ beecrowd-1-iniciante/1589 - Bob Conduite.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cin >> n;
+
+    while (n--){
+        int r1, r2;
+
+        cin >> r1 >> r2;
+
+        cout << r1+r2 << endl;
+    }
+
+    return 0;
+}
+
+
+
+
