c61ff713be123cc30ddcf6aaaf7c34142534af5d
natalete <natsalete14@gmail.com>
2024-02-07 13:24:34 -0300

    Nossos Dias Nunca Voltarão

---
+++ beecrowd-1-iniciante/1864 - Nossos Dias Nunca Voltarão.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int n,i;
+    char v[]="LIFE IS NOT A PROBLEM TO BE SOLVED";
+
+    cin >> n;
+
+    for(i = 0; i < n; i++){
+        cout << v[i];
+    }
+
+    cout << endl;
+
+	return 0;
+}
