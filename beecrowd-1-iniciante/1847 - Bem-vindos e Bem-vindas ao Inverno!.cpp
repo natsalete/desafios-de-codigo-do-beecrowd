e58dc484f799d0bbe8b97460fd4fb79583080129
natalete <natsalete14@gmail.com>
2024-02-07 12:27:53 -0300

    Bem-vindos e Bem-vindas ao Inverno!

---
+++ beecrowd-1-iniciante/1847 - Bem-vindos e Bem-vindas ao Inverno!.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int a , b, c;
+
+    cin >> a >> b >> c;
+
+    if(a > b && (b == c || b < c) || (a < b && b < c  && (c - b >= b - a)) || (a > b && b > c  && (b - c < a - b)) || (a == b && b < c)){
+        cout << ":)" << endl;
+    }else{
+        cout << ":(" << endl;
+    }
+
+
+	return 0;
+}
+
