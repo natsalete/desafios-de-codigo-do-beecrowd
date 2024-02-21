27429f3cb0c41d62173ed3a7c7cd6615b704a3c4
natalete <natsalete14@gmail.com>
2024-02-21 15:07:28 -0300

    Tomadas

---
+++ beecrowd-1-iniciante/1930 - Tomadas.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int t1, t2, t3, t4;
+
+    cin >> t1 >> t2 >> t3 >> t4;
+
+    int maxTOMADAS;
+
+    maxTOMADAS = t1 + t2 + t3 + t4;
+
+    cout << maxTOMADAS - 3 << endl;
+
+    return 0;
+}
