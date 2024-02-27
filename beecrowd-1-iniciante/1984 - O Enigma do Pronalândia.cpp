009057ab0d086f55d5e70c81495552e2fcc9d152
natalete <natsalete14@gmail.com>
2024-02-27 12:52:45 -0300

    O Enigma do Pronalândia

---
+++ beecrowd-1-iniciante/1984 - O Enigma do Pronalândia.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string num;
+
+    cin >> num;
+
+    for(int i = num.size()-1; i >= 0; i--){
+        cout << num[i];
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
