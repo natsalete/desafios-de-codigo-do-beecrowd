8f963ca38185653c05cac4a0015d862c20cf493d
natalete <natsalete14@gmail.com>
2024-06-03 19:37:53 -0300

    Evento

---
+++ beecrowd-1-iniciante/2172 - Evento.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    long long int m;
+
+    while(cin >> x >> m && x != 0 && m != 0){
+        cout << x * m << endl;
+    }
+
+    return 0;
+}
+
