c670629259f596fead1b6b118f657c01a815e5af
natalete <natsalete14@gmail.com>
2024-06-03 20:30:36 -0300

    Feliz Nataaal!

---
+++ beecrowd-1-iniciante/2483 - Feliz Nataaal!.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i;
+
+    cin >> i;
+
+    cout << "Feliz nat";
+
+    while(i--){
+        cout << "a";
+    }
+
+    cout << "l!" << endl;
+
+    return 0;
+}
+
+
