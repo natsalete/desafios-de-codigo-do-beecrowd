5041ecde1694733646e2f3bc96d7cde503d18c28
natalete <natsalete14@gmail.com>
2024-07-04 21:22:21 -0300

    I am Toorg!

---
+++ beecrowd-1-iniciante/2581 - I am Toorg!.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int n;
+    
+    cin >> n;
+    
+    string frase;
+    while(n--){
+        getline(cin, frase);
+        cout << "I am Toorg!" << endl;
+    }
+ 
+    return 0;
+}
