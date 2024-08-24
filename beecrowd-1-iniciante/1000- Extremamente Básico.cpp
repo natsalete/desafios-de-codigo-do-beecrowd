b3d05d2261eacd9bf6a0ace64fb09c720e8a15cf
natalete <natsalete14@gmail.com>
2024-08-23 21:17:20 -0300

    Extremamnte Basico

---
+++ beecrowd-1-iniciante/1000- Extremamente Básico.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int A, B, X;
+
+    cin >> A >> B;
+    //scanf("%d %d", &A, &B);
+    
+    X = A + B;
+    
+    cout << "X = " << X << endl;
+    //printf("X = %d\n", X);
+    
+    return 0;
+}
